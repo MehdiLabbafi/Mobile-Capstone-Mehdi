e18e68e5690fee95b4edc4526c0f6d7946e9f2bd
Mehdi Labbafi <Mehdi.Labbafi@triosstudent.com>
2024-05-31 19:18:06 -0400

    Added folder project

---
+++ HyperMinds/HyperMinds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hyper Minds
+//
+//  Created by Mehdi Labbafi on 2024-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
